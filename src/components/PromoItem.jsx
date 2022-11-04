2899693ba938cf899afffdb696c83a9210c97afa
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-11-04 19:10:12 +0300

    feat: add wide poster

--- src/components/PromoItem.jsx
+++ src/components/PromoItem.jsx
@@ -1,7 +1,12 @@
-import './PromoItem.scss'
+import "./PromoItem.scss";
 
-const PromoItem = ({ item }) => {
-  return <div className="promo-item">PromoItem #{item.id}</div>;
+const PromoItem = ({ item, height, isWide = false }) => {
+  return (
+    <div className="promo-item" style={{ height: `${height}px` }}>
+      <p>PromoItem #{item.id}</p>
+      <p>{isWide ? "WIDE" : "NOT WIDE"}</p>
+    </div>
+  );
 };
 
 export default PromoItem;
--- src/components/PromoList.jsx
+++ src/components/PromoList.jsx
@@ -6,6 +6,7 @@ import PromoItem from "./PromoItem";
 import "swiper/css";
 
 import "./PromoList.scss";
+import { useRef, useState } from "react";
 
 const ITEMS = [
   { id: 1 },
@@ -20,24 +21,124 @@ const ITEMS = [
   { id: 10 },
 ];
 
+const POSTER_WIDTH = 290;
+const POSTER_HEIGHT = 490;
+const POSTER_SPACE_BETWEEN = 30;
+const VIDEO_WIDTH = POSTER_WIDTH * 3 + POSTER_SPACE_BETWEEN * 2;
+const AUTO_VIDEO_DELAY = 3000;
+
 const PromoList = () => {
+  const swiperRef = useRef(null);
+  const autoVideoTimer = useRef(null);
+
+  let inTransition = false;
+  let inTransitionSaveTimerMilliSeconds = 5000;
+  const inTransitionSaveTimer = useRef(null);
+
+  const [currentPosterIndex, setCurrentPosterIndex] = useState(0);
+  const [openedPosterIndex, setOpenedPosterIndex] = useState(0);
+  const [posterType, setPosterType] = useState("poster");
+
+  const handleSlideChangeSlide = (swiper) => {
+    const nextPosterIndex = swiper.realIndex;
+    console.log(swiper);
+    console.log(nextPosterIndex);
+
+    removeAutoVideoTimer();
+
+    setCurrentPosterIndex(nextPosterIndex);
+
+    addAutoVideoTimer(nextPosterIndex);
+  };
+
+  const setInTransition = (value) => {
+    if (value === true) {
+      inTransition = value;
+      const inTransitionOff = () => {
+        if (inTransitionSaveTimer.current) {
+          clearTimeout(inTransitionSaveTimer.current);
+        }
+        inTransition = false;
+      };
+      if (inTransitionSaveTimer.current) {
+        clearTimeout(inTransitionSaveTimer.current);
+      }
+      inTransitionSaveTimer.current = setTimeout(
+        inTransitionOff,
+        inTransitionSaveTimerMilliSeconds
+      );
+    } else if (value === false) {
+      if (inTransitionSaveTimer) clearTimeout(inTransitionSaveTimer);
+      inTransition = false;
+    }
+  };
+
+  const autoVideoHandler = (nextPosterIndex) => {
+    removeAutoVideoTimer();
+    if (nextPosterIndex !== null) {
+      setOpenedPosterIndex(nextPosterIndex);
+
+      console.log("autoVideoHandler");
+      setPosterType("video");
+      setInTransition(true);
+    }
+  };
+
+  const addAutoVideoTimer = (nextPosterIndex) => {
+    if (autoVideoTimer.current) clearTimeout(autoVideoTimer.current);
+    autoVideoTimer.current = setTimeout(() => {
+      autoVideoHandler(nextPosterIndex);
+    }, AUTO_VIDEO_DELAY);
+  };
+
+  const removeAutoVideoTimer = () => {
+    if (autoVideoTimer.current) clearTimeout(autoVideoTimer.current);
+  };
+
+  const onFocusWrapperTransitionEnd = () => {
+    setInTransition(false);
+  };
+
   return (
     <div className="promo-list">
       <Swiper
+        ref={swiperRef}
+        initialSlide={currentPosterIndex}
         modules={[Keyboard]}
-        spaceBetween={30}
+        spaceBetween={POSTER_SPACE_BETWEEN}
         slidesPerView={6}
         keyboard={{ enabled: true }}
         loop
+        onSlideChange={handleSlideChangeSlide}
       >
-        {ITEMS.map((item) => (
-          <SwiperSlide key={item.id}>
-            <PromoItem item={item} />
+        {ITEMS.map((item, index) => (
+          <SwiperSlide
+            style={{
+              width:
+                index === currentPosterIndex && posterType === "video"
+                  ? `${VIDEO_WIDTH}px`
+                  : `${POSTER_WIDTH}px`,
+            }}
+            key={item.id}
+          >
+            <PromoItem
+              item={item}
+              height={POSTER_HEIGHT}
+              isWide={posterType === "video" && index === currentPosterIndex}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
 
-      <div className="promo-list-focus"></div>
+      <div
+        className="promo-list-focus-wrapper"
+        style={{
+          width:
+            posterType === "video" ? `${VIDEO_WIDTH}px` : `${POSTER_WIDTH}px`,
+          height: `${POSTER_HEIGHT}px`,
+        }}
+        onTransitionEnd={onFocusWrapperTransitionEnd}
+      ></div>
     </div>
   );
 };
